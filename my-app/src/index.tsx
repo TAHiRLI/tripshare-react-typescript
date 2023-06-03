05bae7e022038dc964b63065c6d252ac201a4659
TAHiRLI <tahrli.tahirr@gmail.com>
2023-06-03 20:12:26 +0400

    basic dropdown element created closes #9

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -10,13 +10,13 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <>
     <Router>
       <Switch>
         <App />
       </Switch>
     </Router>
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
